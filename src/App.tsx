22a6cd35bb8bc090ddb07b66b042217fb6fb0bf1
tring-bhuvaneshwari <bhuvaneshwari.k@tringapps.com>
2025-04-24 15:49:48 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">PlannyGo</h1>
+      <p className="mt-2 text-gray-700">Plan your perfect outings with friends!</p>
+    </div>
+  )
+}
+
+export default App
